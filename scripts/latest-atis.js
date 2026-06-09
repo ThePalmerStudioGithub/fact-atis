bc48618bbf19bf081c7defe857426a9b8f392a40
The Palmer Studio Open Source <thepalmerstudiogithub@gmail.com>
2026-06-09 18:32:53 +0200

    added a last updated timestamp

--- scripts/latest-atis.js
+++ scripts/latest-atis.js
@@ -12,8 +12,9 @@ var atisdewpoint = "11";
 var atisqnh = "1022 hpa";
 var atissig = false;
 var atissiginfo = "??";
+var lastupdated = "2026/06/09 @ 16:31z";
 document.title = "Cape Town International Airport Information " + atisid;
 var atiscontainer = document.getElementById("ATIS");
 atiscontainer.innerText = document.title + "\n" + "@ " + atiscurrent 
 + "\n" + "Expect vectoring for ILS Approach" + "\n" + "Runway " + atisrunway + " for arrivals and departures" + "\n" +
-"Transition Level " + atistrl + "\n" + "Wind " + atiswind + "\n" + "Visibility " + atisvis + "\n" + "Present Weather " + atispreswx + "\n" + atisclouds + "\n" + "Temperature " + atistemp + "\n" + "Dew Point " + atisdewpoint + "\n" + "QNH " + atisqnh + "\n" + "NO SIGNIFICANT CHANGES" + "\n" + "Confirm " + document.title + " on first contact"
+"Transition Level " + atistrl + "\n" + "Wind " + atiswind + "\n" + "Visibility " + atisvis + "\n" + "Present Weather " + atispreswx + "\n" + atisclouds + "\n" + "Temperature " + atistemp + "\n" + "Dew Point " + atisdewpoint + "\n" + "QNH " + atisqnh + "\n" + "NO SIGNIFICANT CHANGES" + "\n" + "Confirm " + document.title + " on first contact" + "\n" + "Last Updated: " + lastupdated;
